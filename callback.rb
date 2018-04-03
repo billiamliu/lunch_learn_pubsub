c078cc1b9ca0d617380d46b747dfbab819cf58a8
Billiam <billiam.liu@lendesk.com>
2018-04-03 15:34:05 -0700

    AR observer

--- callback.rb
+++ callback.rb
@@ -46,11 +46,61 @@ class AssetPurchase
 
   attr_reader :purchase_price, :funds_required
 
-  before_save :update_property_value
+  after_save :update_property_value
 
   def update_property_value
     asset.property_detail.property_value = purchase_price
 
     loan.cash_to_borrower = loan.amount - purchase_price
+  end
+end
+
+# (m0ar below)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class PropertyValueObserver < ActiveRecord::Observer # convention over configuration
+  def after_save(property_value)
+    if property_value.purchase_price?
+      asset_purchase = property_value.property_detail.asset.asset_purchase
+      asset_purchase.purchase_price = property_value.value
+      asset_purchase.funds_required = property_value.value - asset_purchase.down_payment
+
+      loan = asset_purchase.loan
+      loan.cash_to_borrower = loan.amount - value
+    end
+  end
+end
+
+class AssetPurchaseObserver < ActiveRecord::Observer
+  def after_save(asset_purchase)
+    asset_purchase.asset.property_detail.property_value = asset_purchase.purchase_price
+
+    asset_purchase.loan.cash_to_borrower = loan.amount - asset_purchase.purchase_price
   end
 end
